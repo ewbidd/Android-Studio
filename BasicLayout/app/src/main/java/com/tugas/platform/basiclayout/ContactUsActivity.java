e921950206c8d5960dbdb4cf4cd24c9ed9be7d15
ewbidd <abidd166@gmail.com>
2025-04-27 14:19:29 +0700

    BasicLayout edit

--- BasicLayout/app/src/main/java/com/tugas/platform/basiclayout/ContactUsActivity.java
+++ BasicLayout/app/src/main/java/com/tugas/platform/basiclayout/ContactUsActivity.java
@@ -1,10 +1,12 @@
 package com.tugas.platform.basiclayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,9 +17,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ContactUsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView location, phone, email;
-    private Button btnBack;
+    private TextView location, phone, email, btnBack;
+    private ImageView imgLocation, imgPhone, imgEmail;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +40,16 @@ public class ContactUsActivity extends AppCompatActivity implements View.OnClick
         location = findViewById(R.id.textLocation);
         phone = findViewById(R.id.textPhone);
         email = findViewById(R.id.textEmail);
+        imgLocation = findViewById(R.id.imgLocation);
+        imgPhone = findViewById(R.id.imgPhone);
+        imgEmail = findViewById(R.id.imgEmail);
 
         location.setOnClickListener(this);
         phone.setOnClickListener(this);
         email.setOnClickListener(this);
+        imgLocation.setOnClickListener(this);
+        imgPhone.setOnClickListener(this);
+        imgEmail.setOnClickListener(this);
     }
 
     @Override
@@ -48,19 +57,19 @@ public class ContactUsActivity extends AppCompatActivity implements View.OnClick
 
         int id = v.getId();
 
-        if (id == R.id.textLocation) {
+        if (id == R.id.textLocation || id == R.id.imgLocation) {
             Intent loc = new Intent(Intent.ACTION_VIEW,
                     Uri.parse("google.navigation:q=" + location.getText().toString()));
             startActivity(loc);
 
-        } else if (id == R.id.textEmail) {
+        } else if (id == R.id.textEmail || id == R.id.imgEmail) {
             Intent mail = new Intent(Intent.ACTION_SENDTO,
                     Uri.fromParts("mailto", email.getText().toString(), null));
             mail.putExtra(Intent.EXTRA_SUBJECT, "Subjek");
             mail.putExtra(Intent.EXTRA_TEXT, "Isi Pesan");
             startActivity(mail);
 
-        } else if (id == R.id.textPhone) {
+        } else if (id == R.id.textPhone || id == R.id.imgPhone) {
             Intent call = new Intent(Intent.ACTION_DIAL);
             call.setData(Uri.parse("tel:" + phone.getText().toString()));
             startActivity(call);
