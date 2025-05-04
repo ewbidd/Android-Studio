99b57ccd7d23901a9c5271c8b4b632056f15d7f1
ewbidd <abidd166@gmail.com>
2025-05-05 01:24:27 +0700

    add GridView project

---
+++ GridView/app/src/main/java/com/tugas/platform/gridview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.tugas.platform.gridview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private GridView gridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = findViewById(R.id.gridview);
+        gridView.setAdapter(new GridAdapter(MainActivity.this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(MainActivity.this, "Menu No : " + position, Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), DetailActivity.class);
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+}
